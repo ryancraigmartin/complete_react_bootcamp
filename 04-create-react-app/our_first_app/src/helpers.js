59f02973095fcc83554d48a9524a288dc27b1929
RyanCraigMartin <rmart106@fiu.edu>
2019-09-16 16:27:57 -0400

    S4_module_exports

---
+++ 04-create-react-app/our_first_app/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+  console.log("This was helpful!")
+}
+
+function sort() {
+  console.log("This was sorted!")
+}
+
+function sing() {
+  console.log("LA DEE DA")
+}
+
+export default helpful
+export {sort, sing}
