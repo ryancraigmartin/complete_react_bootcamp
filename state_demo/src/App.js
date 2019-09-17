d73af4b51f1494ff54d452965fda837fe6844246
ryancraigmartin <ryan@ryanmartin.io>
2019-09-17 14:21:44 -0400

    s6_state_demo

---
+++ state_demo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Rando from './Rando';
+// import Demo from './Demo';
+// import Game from './Game';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Rando maxNum={7}/>
+    </div>
+  );
+}
+
+export default App;
