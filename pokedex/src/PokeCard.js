07302645360cbf5a30b251d3871fe019e4f8e6d4
ryancraigmartin <ryan@ryanmartin.io>
2019-09-17 11:44:40 -0400

    s5_pokedex part 3

--- pokedex/src/PokeCard.js
+++ pokedex/src/PokeCard.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './Pokecard.css'
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+let padToThree  = (num) => (num <= 999 ? `00${num}`.slice(-3) : num)
+
 export class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
@@ -12,7 +15,7 @@ export class Pokecard extends Component {
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
     )
-  }
+}
 }
 
 export default Pokecard
--- pokedex/src/Pokegame.js
+++ pokedex/src/Pokegame.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { Pokecard  } from "./Pokecard";
-import "./Pokedex.css";
+import Pokedex from "./Pokedex";
 
-export class Pokedex extends Component {
+export class Pokegame extends Component {
   static defaultProps = {
     pokemon: [
       { id: 4, name: 'Charmander', type: 'fire', exp: 62 },
@@ -16,20 +15,22 @@ export class Pokedex extends Component {
     ]
   }
   render() {
+    let hand1 = []
+    let hand2 = [...this.props.pokemon]
+    while(hand1.length < hand2.length) {
+      let i = Math.floor(Math.random() * hand2.length);
+      let randPokemon = hand2.splice(i, 1)[0]
+      hand1.push(randPokemon)
+    }
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.exp, 0)
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.exp, 0)
     return (
-      <div className="Pokedex">
-        <p>Total EXP: {this.props.exp}</p>
-        <div className="Pokedex-cards">
-        <p>
-          <b>{this.props.isWinner ? "Winner!" : "Loser!"}</b>
-        </p>
-        {this.props.pokemon.map((p) => (
-          <Pokecard id={p.id} name={p.name} type={p.type} exp={p.exp}/>
-        ))}
-        </div>
+      <div>
+        <Pokedex pokemon={hand1} isWinner={exp1 > exp2} exp={exp1}/>
+        <Pokedex pokemon={hand2} isWinner={exp2 > exp1} exp={exp2}/>
       </div>
     )
   }
 }
 
-export default Pokedex
+export default Pokegame
